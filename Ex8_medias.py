db92cb5e0507af64ed3abca9c59f3f6fba19882e
ehasobe <eric_hasobe@hotmail.com>
2015-04-15 09:07:09 -0300

    exercicios

---
+++ Ex8_medias.py
@@ -0,0 +1,33 @@
+import os
+x = []
+soma = 0
+y = 0
+i=1
+
+def maior(x):
+    aux = 0
+    for i in range(len(x)):
+        if aux < x[i]:
+            aux = x[i]
+    return aux
+
+while (y >= 0):
+     while True:
+          try:
+               y = float(input('digite um numero: '))
+               break
+          except:
+               print('numero invalido')
+     if y>=0:
+          x.append(y)
+os.system('cls')
+if len(x)==0:
+     print ('nenhum valor inserido')
+else:
+     for i in range(0,len(x)):
+          print ('nota [' ,i+1, ']: ',x[i])
+          soma = soma + x[i]
+     media = soma/len(x)
+     print('maior nota: ',maior(x))
+     print ('média: ', media)
+input()
